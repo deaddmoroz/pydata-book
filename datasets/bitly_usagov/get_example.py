884ab29e6a70a4e76bd55eaf45e4b6a90305a2ac
deaddmoroz <alxndr.morozov@mail.ru>
2022-06-13 18:42:10 +0300

    usa gov byt.ly

--- datasets/bitly_usagov/get_example.py
+++ datasets/bitly_usagov/get_example.py
@@ -1,9 +1,10 @@
 import os
+import pathlib as p
 
 dir_path = r''
 if __name__ == '__main__':
     x_file = os.path.join(os.getcwd(), "example.txt")
 else:
     x_file = os.path.join(
-        r'C:\Users\morozoval\OneDrive\PythonProjects\pydata-book\datasets\bitly_usagov', "example.txt"
+        p.Path(__file__).parent.resolve(), "example.txt"
     )
